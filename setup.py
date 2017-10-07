c7d4d41968b0d227e5bc81c6b2137360e87284de
Gerardo Castillo <32140109+altersis@users.noreply.github.com>
2017-10-07 10:15:22 -0400

    Version bump, updated setup.py to accommodate changes in the PYPI API (#9)
    Added Long description to read the readme file, changed description to be really short with no line breaks, bumped version to be able to build and send to PYPI

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ CURRENT_DIR = os.path.dirname(__file__)
 # python setup.py register sdist upload
 # and be sure to test it firstly using "python setup.py register sdist upload -r pypitest"
 setup(name='russound',
-      # version=open(os.path.join(CURRENT_DIR, 'xgboost/VERSION')).read().strip(),
-      version='0.1.8',
-      description=open(os.path.join(CURRENT_DIR, 'README.md')).read(),
+      description='Python API for select Russound RNET commands to provide Russound support within home-assistant.io',
+      version='0.1.9',
+      long_description=open(os.path.join(CURRENT_DIR, 'README.md')).read(),
       install_requires=['requests'],
       maintainer='Neil Lathwood',
       maintainer_email='neil@lathwood.co.uk',
